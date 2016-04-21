013f3d2d5f3198dc98b819efa869fb28a8fe2ac1
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-04-21 11:38:16 +0100

    Add test cases

---
+++ tests/src/Calendar-specs.js
@@ -0,0 +1,19 @@
+/* eslint-env jasmine */
+
+describe('An instance of the Calendar class should', () => {
+
+  // ===================
+  // Presentation
+  // ===================
+  xit('create a title element with the appropriate content form the config');
+  xit('create a container for day elements');
+
+  // ===================
+  // Functionality
+  // ===================
+  xit('throw an error when initialised without a config');
+  xit('be instantiated without any days');
+  xit('add the correct number of days when requested');
+  xit('remove the correct number of days when requested');
+  xit('change the date of all days when start date is changed');
+});
---
+++ tests/src/MultiCalendar.js
@@ -0,0 +1,15 @@
+/* eslint-env jasmine */
+
+describe('An instance of the MultiCalendar class should', () => {
+  // ===================
+  // Presentation
+  // ===================
+  xit('create a wrapper for calendars');
+
+  // ===================
+  // Functionality
+  // ===================
+  xit('create the appropriate number of calendars on initilisation');
+  xit('load data from given URL on initialisation');
+  xit('pass loaded data to Calendars correctly');
+});
